2ee28246a1e72612ff617b183383c251087d6378
Robert Van Voorhees <rcvanvo@gmail.com>
2016-04-13 15:47:44 -0400

    Initial import from cluster.

---
+++ src/main/java/net/smartcosmos/events/EventResource.java
@@ -0,0 +1,47 @@
+package net.smartcosmos.events;
+
+import javax.validation.Valid;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Rather than implement an Event listener in every since RDAO, which would cause massive
+ * issues on changing out the event service, the "passive" services that merely send
+ * events can just post to this endpoint.
+ *
+ * @author voor
+ */
+@RestController
+@Slf4j
+public class EventResource {
+
+	private final ISmartCosmosEventGateway eventGateway;
+
+	@Autowired
+	public EventResource(final ISmartCosmosEventGateway eventGateway) {
+		this.eventGateway = eventGateway;
+	}
+
+	@RequestMapping
+	public ResponseEntity getEvent() {
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "**", method = RequestMethod.POST)
+	public ResponseEntity postEvent(@RequestBody @Valid SmartCosmosEvent event) {
+
+		log.info("Received event of type {}, sending event contents: {}",
+				event.getEventType(), event.getData());
+
+		eventGateway.convertAndSend(event);
+
+		return ResponseEntity.noContent().build();
+	}
+}
---
+++ src/main/java/net/smartcosmos/events/EventService.java
@@ -0,0 +1,20 @@
+package net.smartcosmos.events;
+
+import net.smartcosmos.spring.EnableSmartCosmos;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ * @author voor
+ */
+@EnableSmartCosmos
+@SmartCosmosEventProducer
+@SpringBootApplication
+public class EventService {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(EventService.class).web(true).run(args);
+	}
+
+}
