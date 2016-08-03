35fcddac14089679d529fae50cb26a7e908b7d24
Robert Van Voorhees <rcvanvo@gmail.com>
2016-08-03 19:40:33 -0400

    Removes deprecated annotations.

--- src/main/java/net/smartcosmos/events/EventApplication.java
+++ src/main/java/net/smartcosmos/events/EventApplication.java
@@ -1,15 +1,14 @@
 package net.smartcosmos.events;
 
-import net.smartcosmos.spring.EnableSmartCosmos;
-import net.smartcosmos.spring.EnableSmartCosmosSecurity;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import net.smartcosmos.annotation.EnableSmartCosmosEvents;
+import net.smartcosmos.annotation.EnableSmartCosmosSecurity;
 
 /**
  * Launch class for the Event Service. Receives events through regular service authentication, and deposits them into the event pipeline.
  */
-@EnableSmartCosmos
 @EnableSmartCosmosEvents
 @EnableSmartCosmosSecurity
 @SpringBootApplication
