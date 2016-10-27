00f44481f5a5e6737c29c4c2821fd16e62524780
Robert Van Voorhees <rcvanvo@gmail.com>
2016-10-27 15:22:58 +0200

    Refactors test to use MockMvc.

--- src/test/java/net/smartcosmos/events/EventApplicationTest.java
+++ src/test/java/net/smartcosmos/events/EventApplicationTest.java
@@ -1,57 +1,86 @@
 package net.smartcosmos.events;
 
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.builder.RequestSpecBuilder;
-import com.jayway.restassured.http.ContentType;
-import com.jayway.restassured.specification.RequestSpecification;
-
 import org.junit.*;
-import org.junit.runner.*;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.IntegrationTest;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.kafka.test.rule.KafkaEmbedded;
+import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import static com.jayway.restassured.RestAssured.basic;
-import static com.jayway.restassured.RestAssured.given;
+import net.smartcosmos.test.security.WithMockSmartCosmosUser;
 
-/**
- * @author voor
- */
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = { EventApplication.class })
 @ActiveProfiles("test")
 @WebAppConfiguration
-@IntegrationTest({ "spring.cloud.config.enabled=false", "eureka.client.enabled:false" })
+@WithMockSmartCosmosUser
 public class EventApplicationTest {
 
     @ClassRule
     public static KafkaEmbedded embeddedKafka = new KafkaEmbedded(1);
-
-    @Value("${local.server.port}")
-    private int port;
-
-    private RequestSpecification requestSpecification;
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
         System.setProperty("kafka.broker.address", embeddedKafka.getBrokersAsString());
     }
 
-    @Before
-    public void setUp() throws Exception {
-        RestAssured.port = port;
-        RestAssured.authentication = basic("user", "password");
-        RequestSpecBuilder builder = new RequestSpecBuilder();
-        builder.setContentType(ContentType.JSON);
+    protected MediaType contentType = MediaType.APPLICATION_JSON_UTF8;
 
-        requestSpecification = builder.build();
+    @Autowired
+    protected WebApplicationContext webApplicationContext;
+
+    protected MockMvc mockMvc;
+    protected HttpMessageConverter mappingJackson2HttpMessageConverter;
+
+    @Autowired
+    void setConverters(HttpMessageConverter<?>[] converters) {
+
+        this.mappingJackson2HttpMessageConverter = Arrays.stream(converters)
+            .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
+            .findAny()
+            .get();
+
+        Assert.assertNotNull("the JSON message converter must not be null",
+                             this.mappingJackson2HttpMessageConverter);
+    }
+
+    @Before
+    public void setup() throws Exception {
+
+        MockitoAnnotations.initMocks(getClass());
+
+        this.mockMvc = MockMvcBuilders
+            .webAppContextSetup(webApplicationContext)
+            .apply(springSecurity())
+            .build();
+    }
+
+    protected String json(Object o) throws IOException {
+
+        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+        this.mappingJackson2HttpMessageConverter.write(o, MediaType.APPLICATION_JSON,
+                                                       mockHttpOutputMessage);
+        return mockHttpOutputMessage.getBodyAsString();
     }
 
     @After
@@ -65,7 +94,7 @@ public class EventApplicationTest {
     }
 
     @Test
-    public void thatYouCanSendEvent() {
+    public void thatYouCanSendEvent() throws Exception {
         //
         // test that basic object creation works
         //
@@ -73,15 +102,11 @@ public class EventApplicationTest {
         eventBody.put("eventType", "stupid-event");
         eventBody.put("data", "frog");
 
-        // @formatter:off
-        given()
-            .spec(requestSpecification)
-            .body(eventBody)
-            .when()
-            .post("/")
-            .then()
-            .statusCode(204)
-            .log();
-        // @formatter:on
+        MvcResult mvcResult = this.mockMvc.perform(
+            post("/someType/someUrn").content(json(eventBody))
+                .contentType(contentType))
+            .andExpect(status().isNoContent())
+            .andReturn();
+
     }
 }
